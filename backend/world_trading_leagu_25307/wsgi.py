8717fb4a86b6a7b2fd4614d433b402e011d1557c
Crowdbotics <team@crowdbotics.com>
2021-03-27 13:27:08 +0000

    Initial commit.

---
+++ backend/world_trading_leagu_25307/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for world_trading_leagu_25307 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'world_trading_leagu_25307.settings')
+
+application = get_wsgi_application()
