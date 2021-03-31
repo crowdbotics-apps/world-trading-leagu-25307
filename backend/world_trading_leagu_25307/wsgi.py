d6ef99d28802a40f40ce226455d25f7a1640a9ea
Crowdbotics <team@crowdbotics.com>
2021-03-31 18:03:39 +0000

    Update models

--- backend/world_trading_leagu_25307/wsgi.py
+++ backend/world_trading_leagu_25307/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'world_trading_leagu_25307.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "world_trading_leagu_25307.settings")
 
 application = get_wsgi_application()
